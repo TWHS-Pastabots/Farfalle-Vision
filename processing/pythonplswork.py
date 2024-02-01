0edc89a25b291dc5174ba26771f27929cdf79b64
Mark Antonov <113800191+ComradeHibiscus@users.noreply.github.com>
2024-02-01 08:53:37 -0600

    current state of code
    Fixed bug that slowed down apriltag detection

--- processing/pythonplswork.py
+++ processing/pythonplswork.py
@@ -34,18 +34,19 @@ cam1_input_stream = cs.getVideo(camera = usb1)
 cam2_input_stream = cs.getVideo(camera = usb2)
 
 #mjpeg server for cam2 raw stream display
-mj = MJServer("cam2_stream", 1185)
-mj.setSource(usb2)
+mj = MJServer("cam1_stream", 1185)
+mj.setSource(usb1)
+mjstring1 = "mjpeg:http://visiontwhs.local:1185/?action=stream"
 
 #setting up second mjpeg server sink -> source -> mjepg server for cam2
 cam2_sink = sink("cam2_sink")
 cam2_sink.setSource(usb2)
 
-cam2_source = source("cam2_source", pixelFormat = VideoMode.PixelFormat.kMJPEG, width = width, height = height, fps  = 10)
-mj2 = MJServer("cam2_processed", 1186)
-mj2.setSource(cam2_source)
+#cam2_source = source("cam2_source", pixelFormat = VideoMode.PixelFormat.kMJPEG, width = width, height = height, fps  = 10)
+#mj2 = MJServer("cam2_processed", 1186)
+#mj2.setSource(cam2_source)
 
-time.sleep(2)
+time.sleep(1)
 
 output_stream = cs.putVideo(name = 'Cam Stream', width = width,  height = height)
 img = np.zeros(shape = (height, width, 3), dtype = np.uint8)
@@ -57,7 +58,7 @@ np.set_printoptions(suppress = True)
 modelPath = "/home/vision/Documents/Code/best.pt"
 model = YOLO(modelPath, task = "detect")
 
-time.sleep(2)
+time.sleep(0.5)
 
 # Checks if a matrix is a valid rotation matrix.
 def isRotationMatrix(R):
@@ -87,133 +88,130 @@ def rotationMatrixToEulerAngles(R):
 
 #detects apriltags for cam1
 def cam1TagDetect():
+    while True:
+        cam1_frame_time, cam1_input_img = cam1_input_stream.grabFrame(img)
 
-    cam1_frame_time, cam1_input_img = cam1_input_stream.grabFrame(img)
+        if cam1_frame_time == 0:
+            output_stream.notifyError(cam1_input_stream.getError())
 
-    if cam1_frame_time == 0:
-        output_stream.notifyError(cam1_input_stream.getError())
+        vision_table.putString("Cam1 Stream", mjstring1)
 
-    #setting up tag info lists           
-    x_list = []
-    y_list = []
-    id_list = []
-    x_euler_list = []
-    y_euler_list = []
-    z_euler_list = []
+        #setting up tag info lists           
+        x_list = []
+        y_list = []
+        id_list = []
+        x_euler_list = []
+        y_euler_list = []
+        z_euler_list = []
 
-    #set up tag detector
-    detector = robotpy_apriltag.AprilTagDetector()
-    detector.addFamily("tag16h5")
-    DETECTION_MARGIN_THRESHOLD = 100
+        #set up tag detector
+        detector = robotpy_apriltag.AprilTagDetector()
+        detector.addFamily("tag16h5")
+        DETECTION_MARGIN_THRESHOLD = 100
 
-    #grayscale frame + detect
-    gray_img = cv2.cvtColor(cam1_input_img, cv2.COLOR_BGR2GRAY)
-    tag_info = detector.detect(gray_img)
+        #grayscale frame + detect
+        gray_img = cv2.cvtColor(cam1_input_img, cv2.COLOR_BGR2GRAY)
+        tag_info = detector.detect(gray_img)
 
-    #filter out bad detections (low decision margin + out of bounds IDs)
-    filter_tags = [
-        tag for tag in tag_info if tag.getDecisionMargin() > DETECTION_MARGIN_THRESHOLD]
-    filter_tags = [tag for tag in filter_tags if (
-        (tag.getId() > 0) & (tag.getId() < 16))]
+        #filter out bad detections (low decision margin + out of bounds IDs)
+        filter_tags = [
+            tag for tag in tag_info if tag.getDecisionMargin() > DETECTION_MARGIN_THRESHOLD]
+        filter_tags = [tag for tag in filter_tags if (
+            (tag.getId() > 0) & (tag.getId() < 16))]
 
-    #send detections info over network tables
-    for tag in filter_tags:
-        tag_id = tag.getId()
-        center = tag.getCenter()
-        homography = tag.getHomographyMatrix()
-        euler_list = rotationMatrixToEulerAngles(homography)
+        #send detections info over network tables
+        for tag in filter_tags:
+            tag_id = tag.getId()
+            center = tag.getCenter()
+            homography = tag.getHomographyMatrix()
+            euler_list = rotationMatrixToEulerAngles(homography)
 
-        x_list.insert(0, (center.y - width / 2) / (width / 2) * 1000)
-        y_list.insert(0, (center.x - width / 2) / (width / 2) * 1000)
-        id_list.insert(0, tag_id * 1000)
-        z_euler_list.insert(0, euler_list[2] * 1000)
-        # x_euler_list.append(euler_list[0])
-        # y_euler_list.append(euler_list[1])
+            x_list.insert(0, (center.y - width / 2) / (width / 2) * 1000)
+            y_list.insert(0, (center.x - width / 2) / (width / 2) * 1000)
+            id_list.insert(0, tag_id * 1000)
+            z_euler_list.insert(0, euler_list[2] * 1000)
+            # x_euler_list.append(euler_list[0])
+            # y_euler_list.append(euler_list[1])
 
-        vision_table.putNumberArray("IDs", id_list)
-        vision_table.putNumberArray("X Coords", x_list)
-        vision_table.putNumberArray("Y Coords", y_list)
-        vision_table.putNumberArray("Z Euler Angles", z_euler_list)
-        # vision_table.putNumberArray("X Euler Angles", x_euler_list)
-        # vision_table.putNumberArray("Y Euler Angles", y_euler_list)
+            vision_table.putNumberArray("IDs", id_list)
+            vision_table.putNumberArray("X Coords", x_list)
+            vision_table.putNumberArray("Y Coords", y_list)
+            vision_table.putNumberArray("Z Euler Angles", z_euler_list)
+            # vision_table.putNumberArray("X Euler Angles", x_euler_list)
+            # vision_table.putNumberArray("Y Euler Angles", y_euler_list)
 
-        #pop lists in case they get too big to avoid memory issues
-        if len(x_list) > 10:
-           x_list.pop()
+            #pop lists in case they get too big to avoid memory issues
+            if len(x_list) > 10:
+                x_list.pop()
 
-        if len(y_list) > 10:
-           y_list.pop()
+            if len(y_list) > 10:
+                y_list.pop()
 
-        if len(id_list) > 5:
-           x_list.pop()
+            if len(id_list) > 5:
+                x_list.pop()
 
-        if len(z_euler_list) > 10:
-           x_list.pop()
+            if len(z_euler_list) > 10:
+                x_list.pop()
 
         #print(x_euler_list)
         #print(y_euler_list)
         #print(z_euler_list)
-        #print(id_list)
+        print(id_list)
         #print(x_list)
         #print(y_list)
         #print(z_euler_list)
         #print("\n")
-           
-def cam2RingDetect():
-    cam2_frame_time, cam2_input_img = cam2_sink.grabFrame(img)
-
-
-    if cam2_frame_time == 0:
-        output_stream.notifyError(cam2_input_stream.getError())
-
-    ring_center_x = []
-    ring_center_y = []
-    results = model.predict(source = cam2_input_img, conf = 0.25, save = False)
-    
-    #make results workable
-    np_results = results[0]
-        
-    #mark ring contours
-    if len(np_results) == 0:
-        print("No Ring")
-    else:
-        for i in range(len(np_results.boxes)):
-            box = np_results.boxes.xyxy[i]
-            l = int(box[0])
-            t = int(box[1])
-            b = int(box[2])
-            r =  int(box[3])
   
-            #image, (topleft), (bottomright), color, thickness px
-            cv2.rectangle(cam2_input_img, (l, t), (b, r), (0, 255, 0), 3)
-            ring_center_x.append((l + r) / 2.0)
-            ring_center_y.append((b + t) / 2.0)
-        
-        vision_table.putNumberArray("Ring Center X Coords", ring_center_x)
-        vision_table.putNumberArray("Ring Center Y Coords", ring_center_y)
+def cam2RingDetect():
+    while True:
+        cam2_frame_time, cam2_input_img = cam2_sink.grabFrame(img)
 
-        if len(ring_center_x) > 5:
-            ring_center_x.pop()
+        if cam2_frame_time == 0:
+            output_stream.notifyError(cam2_input_stream.getError())
 
-        if len(ring_center_y) > 5:
-            ring_center_y.pop()
-        
-        cam2_source.putFrame(cam2_input_img)
-        
-        cv2.imshow("Rings", cam2_input_img)
-        cv2.waitKey(1)
+        #get model resultsSS
+        results = model.predict(source = cam2_input_img, conf = 0.25, save = False)
+        np_results = results[0]
+
+        ring_center_x = []
+        ring_center_y = []
+            
+        #mark ring contours
+        if len(np_results) == 0:
+            print("No Ring")
+        else:
+            for i in range(len(np_results.boxes)):
+                box = np_results.boxes.xyxy[i]
+                l = int(box[0])
+                t = int(box[1])
+                b = int(box[2])
+                r = int(box[3])
+    
+                #image, (topleft), (bottomright), color, thickness px
+                #cv2.rectangle(cam2_input_img, (l, t), (b, r), (0, 255, 0), 3)
+                ring_center_x.append((l + r) / 2.0)
+                ring_center_y.append((b + t) / 2.0)
+            
+            vision_table.putNumberArray("Ring Center X Coords", ring_center_x)
+            vision_table.putNumberArray("Ring Center Y Coords", ring_center_y)
+
+            if len(ring_center_x) > 5:
+                ring_center_x.pop()
+
+            if len(ring_center_y) > 5:
+                ring_center_y.pop()
+            
+            #cam2_source.putFrame(cam2_input_img)
+            
+            #cv2.imshow("Rings", cam2_input_img)
+            #cv2.waitKey(1)
 
 def main():
-    while True:        
         #set up threads and run
-        t1 = threading.Thread(target = cam1TagDetect(), name = "cam1 thead")
-        t2 = threading.Thread(target = cam2RingDetect(), name = "cam2 thread")
-        
-        t1.start()
-        t2.start()
+        t1 = threading.Thread(target = cam1TagDetect, name = "cam1 thead")
+        t2 = threading.Thread(target = cam2RingDetect, name = "cam2 thread")
 
-        #wait till both threads are finished
-        t1.join()
-        t2.join()
+        t2.start()
+        t1.start()      
 
 main()
