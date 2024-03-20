a6059da0924dacfbb641a8264997846eb97778a0
Mark Antonov <113800191+ComradeHibiscus@users.noreply.github.com>
2024-03-19 19:25:18 -0500

    4 cams on pi

--- processing/pythonplswork.py
+++ processing/pythonplswork.py
@@ -4,11 +4,8 @@ import threading
 import numpy as np
 from cscore import CameraServer as cs
 from cscore import VideoSource as vs
-#from cscore import CvSink as sink
 import cv2
-#from ultralytics import YOLO
 import robotpy_apriltag as rptag
-#from wpimath.geometry import Transform3d, Translation3d, Rotation3d
 from ntcore import NetworkTableInstance as nt
 import ntcore
 
@@ -25,31 +22,36 @@ height = 480
 fps = 15
 
 # Set up cameras
-usb1 = cs.startAutomaticCapture(name = "cam1", path = "/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.1:1.0-video-index0")
+usb1 = cs.startAutomaticCapture(name = "Launcher Cam", path = "/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.1:1.0-video-index0")
 usb1.setConnectionStrategy(vs.ConnectionStrategy.kConnectionKeepOpen)
 usb1.setResolution(width, height)
 usb1.setFPS(fps)
 
-usb2 = cs.startAutomaticCapture(name = "cam2", path = "/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2:1.0-video-index0")
+usb2 = cs.startAutomaticCapture(name = "Left Cam", path = "/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2:1.0-video-index0")
 usb2.setConnectionStrategy(vs.ConnectionStrategy.kConnectionKeepOpen)
 usb1.setResolution(width, height)
 usb2.setFPS(fps)
 
-# Insert dev path later
-usb3 = cs.startAutomaticCapture(name = "cam3", path = "/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4:1.0-video-index0")
+usb3 = cs.startAutomaticCapture(name = "Right Cam", path = "/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4:1.0-video-index0")
 usb3.setConnectionStrategy(vs.ConnectionStrategy.kConnectionKeepOpen)
 usb3.setResolution(width, height)
 usb3.setFPS(fps)
 
+usb4 = cs.startAutomaticCapture(name = "Intake Cam", path = "/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-video-index0")
+usb4.setConnectionStrategy(vs.ConnectionStrategy.kConnectionKeepOpen)
+usb4.setResolution(width, height)
+usb4.setFPS(fps)
+
 cam1_input_stream = cs.getVideo(camera = usb1)
 cam2_input_stream = cs.getVideo(camera = usb2)
 cam3_input_stream = cs.getVideo(camera = usb3)
+cam4_input_stream = cs.getVideo(camera = usb4)
 
 # Setting up sink for cam2
 #cam2_sink = sink("cam2_sink")
 #cam2_sink.setSource(usb2)
 
-time.sleep(0.5)
+time.sleep(0.05)
 
 output_stream = cs.putVideo(name = 'Cam Stream', width = width,  height = height)
 img = np.zeros(shape = (height, width, 3), dtype = np.uint8)
@@ -122,10 +124,9 @@ time.sleep(0.5)
 
 # Detects apriltags for cam1
 def cam1TagDetect():
-    while True:
         cam1_frame_time, cam1_input_img = cam1_input_stream.grabFrame(img)
 
-        if cam1_frame_time == 0:
+        if cam1_frame_time == 0:#If frame time is zero then there was no time between the last frame so no new data
             output_stream.notifyError(cam1_input_stream.getError())
 
         # Setting up tag info lists           
@@ -170,10 +171,10 @@ def cam1TagDetect():
             # homography = tag.getHomographyMatrix()
             # euler_list = rotationMatrixToEulerAngles(homography)
             tag_pos = tag_estimator.estimate(tag)
-            x_list.insert(0, tag_pos.getX())
-            y_list.insert(0, tag_pos.getY())
-            z_list.insert(0, tag_pos.getZ())
-            yaw_list.insert(0, tag_pos.getRotation().getZ())
+            x_list.insert(0, tag_pos.X())
+            y_list.insert(0, tag_pos.Y())
+            z_list.insert(0, tag_pos.Z())
+            yaw_list.insert(0, tag_pos.rotation().Z())
             timestamp_list.insert(0, ntcore._now())
             # x_list.insert(0, (center.y))
             # y_list.insert(0, (center.x))
@@ -197,6 +198,7 @@ def cam1TagDetect():
 
             if len(id_list) > 10:
                 id_list.pop()
+
             if len(timestamp_list) > 10:
                 timestamp_list.pop()
             # if len(z_euler_list) > 10:
@@ -269,7 +271,6 @@ def cam1TagDetect():
 #             #cv2.waitKey(1)
 
 def cam2TagDetect():
-    while True:
         cam2_frame_time, cam2_input_img = cam2_input_stream.grabFrame(img)
 
         if cam2_frame_time == 0:
@@ -317,10 +318,10 @@ def cam2TagDetect():
             # homography = tag.getHomographyMatrix()
             # euler_list = rotationMatrixToEulerAngles(homography)
             tag_pos = tag_estimator.estimate(tag)
-            x_list.insert(0, tag_pos.getX())
-            y_list.insert(0, tag_pos.getY())
-            z_list.insert(0, tag_pos.getZ())
-            yaw_list.insert(0, tag_pos.getRotation().getZ())
+            x_list.insert(0, tag_pos.X())
+            y_list.insert(0, tag_pos.Y())
+            z_list.insert(0, tag_pos.Z())
+            yaw_list.insert(0, tag_pos.rotation().Z())
             timestamp_list.insert(0, ntcore._now())
             # x_list.insert(0, (center.y))
             # y_list.insert(0, (center.x))
@@ -374,7 +375,6 @@ def cam2TagDetect():
 
 #detects apriltags for cam1
 def cam3TagDetect():
-    while True:
         cam3_frame_time, cam3_input_img = cam3_input_stream.grabFrame(img)
 
         if cam3_frame_time == 0:
@@ -422,10 +422,10 @@ def cam3TagDetect():
             # homography = tag.getHomographyMatrix()
             # euler_list = rotationMatrixToEulerAngles(homography)
             tag_pos = tag_estimator.estimate(tag)
-            x_list.insert(0, tag_pos.getX())
-            y_list.insert(0, tag_pos.getY())
-            z_list.insert(0, tag_pos.getZ())
-            yaw_list.insert(0, tag_pos.getRotation().getZ())
+            x_list.insert(0, tag_pos.X())
+            y_list.insert(0, tag_pos.Y())
+            z_list.insert(0, tag_pos.Z())
+            yaw_list.insert(0, tag_pos.rotation().Z())
             timestamp_list.insert(0, ntcore._now())
             # x_list.insert(0, (center.y))
             # y_list.insert(0, (center.x))
@@ -478,13 +478,20 @@ def cam3TagDetect():
         #     vision_table.putNumber("DistanceToTag", distance)
 
 def main():
-        #set up threads and run
-        t1 = threading.Thread(target = cam1TagDetect, name = "cam1 thread")
-        t2 = threading.Thread(target = cam2TagDetect, name = "cam2 thread")
-        t3 = threading.Thread(target = cam3TagDetect, name = "cam3 thread")
+        # #set up threads and run
+        # t1 = threading.Thread(target = cam1TagDetect, name = "cam1 thread")
+        # t2 = threading.Thread(target = cam2TagDetect, name = "cam2 thread")
+        # t3 = threading.Thread(target = cam3TagDetect, name = "cam3 thread")
 
-        t1.start()
-        t2.start()      
-        t3.start()
+        # t1.start()
+        # t2.start()      
+        # t3.start()
+    while True:
+        try:
+            cam1TagDetect()
+            cam2TagDetect()
+            cam3TagDetect()
+        except:
+            continue
 
 main()
